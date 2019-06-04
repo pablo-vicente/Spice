a69b46b711917a615d0df267c242e17c420fb734
Pablo Vicente <pablo.vicente@grad.ufsc.br>
2019-06-04 14:11:55 -0300

    Create Model Coupon

--- Spice/Data/ApplicationDbContext.cs
+++ Spice/Data/ApplicationDbContext.cs
@@ -12,11 +12,11 @@ namespace Spice.Data
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
-
         }
 
         public DbSet<Category> Category {get; set;}
         public DbSet<SubCategory> SubCategory { get; set; }
         public DbSet<MenuItem> MenuItem { get; set; }
+        public DbSet<Coupon> Coupon { get; set; }
     }
 }
